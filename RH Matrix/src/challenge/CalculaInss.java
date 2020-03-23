c7d391073d2ef7c1600fd05b743caeb69a036651
Larissa dos Santos Alves <46758980+1903larissaalves@users.noreply.github.com>
2020-03-22 21:35:53 -0300

    Adicionando comentarios

--- RH Matrix/src/challenge/CalculaInss.java
+++ RH Matrix/src/challenge/CalculaInss.java
@@ -1,14 +1,18 @@
 package challenge;
 
+//classe filha de calculadora impostos
 public class CalculaInss extends CalculadoraImpostos {
 
+	//calcula desconto de INSS
 	public double calcularImposto(double salarioBase) {
 		
+		//percentual de desconto INSS
 		final double DESCONTO_INSS_SETE_PONTO_CINCO_PORCENTO = 0.075;
 		final double DESCONTO_INSS_NOVE_PORCENTO = 0.09;
 		final double DESCONTO_INSS_DOZE_PORCENTO = 0.12;
 		final double DESCONTO_INSS_QUATORZE_PORCENTO = 0.14;
 				
+		//aplica desconto conforme faixa salarial
 		if (salarioBase <= 1045) {
 			return salarioBase * DESCONTO_INSS_SETE_PONTO_CINCO_PORCENTO;
 			
