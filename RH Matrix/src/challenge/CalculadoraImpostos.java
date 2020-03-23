868af498bbe797ccc94e08eeb02bedc93080c5dd
Larissa dos Santos Alves <46758980+1903larissaalves@users.noreply.github.com>
2020-03-22 21:36:32 -0300

    Adicionando comentarios

--- RH Matrix/src/challenge/CalculadoraImpostos.java
+++ RH Matrix/src/challenge/CalculadoraImpostos.java
@@ -2,7 +2,9 @@ package challenge;
 
 public class CalculadoraImpostos {
 	
+	//classe pai de impostos
 	public double calcularImposto(double salarioBase) {
+		//valor padrao 
 		return salarioBase;
 	}
 }
